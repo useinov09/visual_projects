15babd7097c0c646389b5d247e87fa8fb4456f9f
Edem Useinov <edemuseinov322@gmail.com>
2026-04-20 20:58:23 +0300

    a_bit_of_analysis: Automate earthquake map title from JSON metadata

--- a_bit_of_analysis/eq_world_map.py
+++ a_bit_of_analysis/eq_world_map.py
@@ -3,10 +3,15 @@ import json
 from plotly.graph_objs import Scattergeo, Layout
 from plotly import offline
 
+# Загрузка файла
 filename = 'data/eq_data_30_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
 
+# Получение заголовка
+title = all_eq_data['metadata']['title']
+
+# Сохраняем данные
 all_eq_dicts = all_eq_data['features']
 mags, lons, lats, hover_texts = [], [], [], []
 for eq_dict in all_eq_dicts:
@@ -29,7 +34,7 @@ data = [{
     'colorbar': {'title': 'Magnitude'},
     },
 }]
-my_layout = Layout(title='Global Earthquakes')
+my_layout = Layout(title=title)
 
 fig = {'data': data, 'layout': my_layout}
 offline.plot(fig, filename='visualizations/global_earthquakes.html')
